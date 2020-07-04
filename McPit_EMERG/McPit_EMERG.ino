408225ed317229468a44e60d6d12b33c8c246f84
Talbot McInnis <talbotmcinnis@alumni.uwo.ca>
2020-07-04 16:28:55 -0400

    Enhanced volume controls

--- McPit_EMERG/McPit_EMERG.ino
+++ McPit_EMERG/McPit_EMERG.ino
@@ -1,5 +1,7 @@
+//#define DCSBIOS_IRQ_SERIAL
+#define DCSBIOS_DEFAULT_SERIAL
+
 #include <DcsBios.h>
-#include <Servo.h>
 // For usb_desc.h:
 /*
 #define PRODUCT_ID		0x0487
@@ -18,10 +20,29 @@ DcsBios::Switch2Pos efcpTrimOverride("EFCP_TRIM_OVERRIDE", 11, true);
 
 /**** Volumes ****/
 DcsBios::Switch2Pos intAimUnmute("INT_AIM_UNMUTE", 25);
+DcsBios::Switch2Pos intIffUnmute("INT_IFF_UNMUTE", 25);
+DcsBios::Switch2Pos intIlsUnmute("INT_ILS_UNMUTE", 25);
+DcsBios::Switch2Pos intTcnUnmute("INT_TCN_UNMUTE", 25);
+
 DcsBios::Potentiometer intAimVol("INT_AIM_VOL", 44);
+DcsBios::Potentiometer intIffVol("INT_IFF_VOL", 44);
+DcsBios::Potentiometer intIlsVol("INT_ILS_VOL", 44);
+DcsBios::Potentiometer intTcnVol("INT_TCN_VOL", 44);
+
 DcsBios::Switch2Pos intHm("INT_HM", 16);
+
 DcsBios::Switch2Pos intIntUnmute("INT_INT_UNMUTE", 26);
+DcsBios::Switch2Pos intFmUnmute("INT_FM_UNMUTE", 26);
+DcsBios::Switch2Pos intVhfUnmute("INT_VHF_UNMUTE", 26);
+DcsBios::Switch2Pos intUhfUnmute("INT_UHF_UNMUTE", 26);
+
 DcsBios::Potentiometer intIntVol("INT_INT_VOL", 45);
+DcsBios::Potentiometer intFmVol("INT_FM_VOL", 45);
+DcsBios::Potentiometer intVhfVol("INT_VHF_VOL", 45);
+DcsBios::Potentiometer intUhfVol("INT_UHF_VOL", 45);
+
+DcsBios::Potentiometer intVol("INT_VOL", 45);
+
 const byte intModePins[5] = {12, 13, 14, 15, 36};  // I didn't wire the BLANK spot.   // Note 36 is NC, but I can't wire CENTER pos, so hopefully this works!!!!
 DcsBios::SwitchMultiPos intMode("INT_MODE", intModePins, 5);
 
@@ -36,7 +57,7 @@ DcsBios::Switch2Pos gndSafeOverrideToggle("GND_SAFE_OVERRIDE", 17);
 DcsBios::ProtocolParser parser;
 
 void setup() {
-  Serial.begin(500000);
+  DcsBios::setup();
 }
 
 /*
@@ -48,48 +69,5 @@ to detect changes in the state of connected controls and
 pass them on to DCS.
 */
 void loop() {
-  // feed incoming data to the parser
-  int data = Serial.read();
-  while (data > -1) {
-      parser.processChar(data);
-      data = Serial.read();
-  }
-  
-  // poll inputs
-  DcsBios::PollingInput::pollInputs();
-}
-
-/*
-You need to define
-void sendDcsBiosMessage(const char* msg, const char* arg)
-so that the string msg, followed by a space, the string arg
-and a newline gets sent to the DCS-BIOS import stream.
-
-In this example we send it to the serial port, so you need to
-run socat to read the data from the serial port and send it
-over UDP to DCS-BIOS.
-
-If you are using an Ethernet Shield, you would probably want
-to send a UDP packet from this subroutine.
-*/
-void sendDcsBiosMessage(const char* msg, const char* arg) {
-  Serial.write(msg);
-  Serial.write(' ');
-  Serial.write(arg);
-  Serial.write('\n');
-}
-
-/*
-This subroutine gets called every time a message is received
-from the export stream (you need to define it even if it
-does nothing).
-
-Use this to handle outputs which are not covered by the
-DcsBios Arduino library (e.g. displays).
-*/
-void onDcsBiosWrite(unsigned int address, unsigned int value) {
-}
-
-void onDcsBiosFrameSync()
-{
+  DcsBios::loop();
 }
